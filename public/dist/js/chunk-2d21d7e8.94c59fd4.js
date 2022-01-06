013354cad46abeceb32a0f9efa2bac598b50dbb9
fanjiadong <fanjiadong@bytedance.com>
2022-01-06 17:54:34 +0800

    build: build

--- public/dist/js/chunk-2d21d7e8.94c59fd4.js
+++ public/dist/js/chunk-2d21d7e8.94c59fd4.js
@@ -1,25 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
-  ["chunk-2d21d7e8"],
-  {
-    d215: function (n, e, t) {
-      "use strict";
-      function c(n, e, t, c, r, u) {
-        return "排行榜";
-      }
-      t.r(e);
-      var r = t("5530"),
-        u = t("7a23"),
-        d = Object(u["j"])({
-          setup: function () {
-            var n = Object(u["x"])({});
-            return Object(r["a"])({}, Object(u["F"])(n));
-          },
-        }),
-        o = t("6b0d"),
-        a = t.n(o);
-      const b = a()(d, [["render", c]]);
-      e["default"] = b;
-    },
-  },
-]);
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21d7e8"],{d215:function(n,e,t){"use strict";function c(n,e,t,c,r,u){return"排行榜"}t.r(e);var r=t("5530"),u=t("7a23"),d=Object(u["j"])({setup:function(){var n=Object(u["x"])({});return Object(r["a"])({},Object(u["F"])(n))}}),o=t("6b0d"),a=t.n(o);const b=a()(d,[["render",c]]);e["default"]=b}}]);
 //# sourceMappingURL=chunk-2d21d7e8.94c59fd4.js.map
